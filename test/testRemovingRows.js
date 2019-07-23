f9dbb634e9eeef05e2f914e52120890a14065dc4
lein <leinic@gmail.com>
2019-07-23 21:47:19 +0800

    Fixed spliceRows bug

--- test/testRemovingRows.js
+++ test/testRemovingRows.js
@@ -15,5 +15,5 @@ sheet.spliceRows(2, 1);
 console.log('expects 4, got', sheet.rowCount);
 sheet.spliceRows(2, 0, ['+6666'], ['+7777']);
 console.log('expects 6, got', sheet.rowCount);
-sheet.spliceRows(1, sheet.rowCount);
-console.log('expects 0, got', sheet.rowCount);
+sheet.spliceRows(2, sheet.rowCount);
+console.log('expects 1, got', sheet.rowCount);
